8c76ae980da376c44ffdef2a908535d3fa461d43
NileI <niletingham@gmail.com>
2026-02-26 11:05:21 +0000

    make text red

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+st.markdown(
+    """
+    <style>
+        html, body, [class*="css"]  {
+            color: red !important;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("Welcome to Northeastern University")
 
 user_input = st.text_input("Your name")
@@ -7,4 +18,4 @@ user_input = st.text_input("Your name")
 course_input = st.text_input("What course are you studying?")
 
 if user_input:
-    st.write(f"Hello, {user_input.title()}, good luck studying {course_input.title()}")
+    st.write(f"Hello, {user_input.title()}, good luck studying {course_input.title()}!")
