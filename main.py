c044fbe89b66dd4469a35699b42bcc51dee442b2
NileI <niletingham@gmail.com>
2026-02-26 11:03:12 +0000

    add course

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ st.title("Welcome to Northeastern University")
 
 user_input = st.text_input("Your name")
 
+course_input = st.text_input("What course are you studying?")
+
 if user_input:
-    st.write(f"Hello, {user_input.title()}")
+    st.write(f"Hello, {user_input.title()}, good luck studying {course_input.title()}")
